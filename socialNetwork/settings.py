72a05ee80af0fdeb4817df49f1b5d49991c4d5d8
Raj Mazumder <61340960+ProgrammerRaj-py@users.noreply.github.com>
2021-01-04 17:06:50 +0530

    addded

--- socialNetwork/settings.py
+++ socialNetwork/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '+ud-u-i7@u7t+44c=dj8=l$ww+w3=%!wmlmcalm8dx&i^skq6d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'socialnetworkraj.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
